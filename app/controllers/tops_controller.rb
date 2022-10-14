7d43ff938a334089bd55d78f9cb4a0d408512839
Ubuntu <ubuntu@ip-172-31-55-185.ec2.internal>
2022-10-14 12:21:49 +0000

    Completed 200 OK in 53ms (Views: 43.5ms | ActiveRecord: 0.4ms)

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,8 +4,8 @@ class TopsController < ApplicationController
   end
   
   def login
-    user = User.find_by(uid:params[:uid])
-    if BCrypt::Password.new(user.pass) == params[:pass]
+    user = User.find_by(uid: params[:uid])
+    if user and BCrypt::Password.new(user.pass) == params[:pass]
       session[:login_uid] = params[:uid]
       redirect_to root_path
     else
