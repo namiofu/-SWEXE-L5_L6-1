3f55a8aefce4ff31f7d684dd69197befdae691e7
Ubuntu <ubuntu@ip-172-31-55-185.ec2.internal>
2022-10-19 09:58:58 +0000

    Completed 200 OK in 64ms (Views: 63.5ms | ActiveRecord: 0.0ms)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
     private
         def current_user
-            if session[:login_uid]
-                User.find_by(uid: session[:login_uid])
+            if session[:uid]
+                User.find_by(uid: session[:uid])
             end
         end
         helper_method :current_user
